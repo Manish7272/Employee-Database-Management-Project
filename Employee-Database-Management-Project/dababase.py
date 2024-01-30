f835234362b358590f5e12fd3bda7286acaa65c4
Manish Gupta <71213166+Manish7272@users.noreply.github.com>
2024-01-30 17:06:21 +0530

    Add files via upload

---
+++ Employee-Database-Management-Project/dababase.py
@@ -0,0 +1,20 @@
+import sqlite3 as sqlight3
+
+def create_table():
+    
+
+def fetch_employees():
+  
+
+def insert_employees(id, name, role, gender,status):
+  
+
+def delete_employee(id):
+   
+                   
+def update_employee(new_name, new_role, new_gender, new_status, id):
+  
+
+def id_exists(id):
+  
+create_table()
